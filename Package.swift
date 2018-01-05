81a5d0f542dc4b3bc4f2cff5ad4f300b2ac699cd
Aaron Liberatore <AaronLiberatore@aaron.attlocal.net>
2018-01-05 15:48:04 -0600

    4.0.3

--- Package.swift
+++ Package.swift
@@ -1,9 +1,37 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+/*
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import PackageDescription
 
 let package = Package(
     name: "Aphid",
-    dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 10),
+    products: [
+        .executable(
+            name: "Aphid",
+            targets: ["Aphid"]
+        ),
     ],
-    exclude: ["Aphid.xcodeproj", "README.md", "Sources/Info.plist"]
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Aphid",
+            dependencies: []
+        )
+    ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import AphidTests
-
-XCTMain([
-     testCase(AphidTests.allTests),
-])
